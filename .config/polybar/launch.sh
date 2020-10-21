66ca647f6335829f75421827b797fd246a187a76
Jaimss <james@harrell365.com>
2020-10-21 16:11:26 -0400

    polybar added and some changes

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+polybar main >>/tmp/polybar1.log 2>&1 & disown
+polybar secondary >>/tmp/polybar1.log 2>&1 & disown
+
+# if type "xrandr"; then
+#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#     MONITOR=$m polybar i3 &
+#   done
+# else
+#   polybar i3 &
+# fi
+
+echo "Bars launched..."
