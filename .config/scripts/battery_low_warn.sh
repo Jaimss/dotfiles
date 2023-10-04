b98eb50777340e8b8c1c6b9b495375977aae2865
James Harrell <james@harrell365.com>
2023-10-04 14:52:53 -0400

    Saved Class Notes Wed Oct  4 14:52:00 EDT 2023

--- .config/scripts/battery_low_warn.sh
+++ .config/scripts/battery_low_warn.sh
@@ -1,8 +1,10 @@
-CURR=$(acpi -b | awk '{print $4}' | cut -c -2)
-echo $CURR
+#!/bin/bash
+
+#CURR=$(acpi -b | awk '{print $4}' | cut -c -2)
+CURR=$(acpi -b | awk '{print $4}' | sed -e 's/[^0-9]/ /g')
+#echo $CURR
 if [ $CURR -lt 20 ]; then
-    notify-send -u critical "Low Battery" "Current: ${CURR}%\nFind a charger."
-fi
-if [ $CURR -lt 10 ]; then
-    notify-send -u critical "Low Battery" "Current: ${CURR}%\nFind a charger."
+    /usr/bin/notify-send -u critical "Low Battery" "Current: ${CURR}%\nFind a charger."
+#elif [ $CURR -lt 10 ]; then
+    #/usr/bin/notify-send -u critical "Low Battery" "Current: ${CURR}%\nFind a charger."
 fi
